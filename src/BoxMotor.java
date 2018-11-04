9655bc36dc450206007484e49d8d5c989e654fc3
hp <hp@169.254.43.189>
2018-11-04 19:56:18 +0700

    class BoxMotor

--- src/BoxMotor.java
+++ src/BoxMotor.java
@@ -9,5 +9,52 @@
  * @author Arifannyrs
  */
 public class BoxMotor {
+    private String nama;
+    private float tipe;
+    public float jumlah;
+    private float harga;
+
+    public BoxMotor(String nama, float tipe, float jumlah, float harga) {
+        this.nama = nama;
+        this.tipe = tipe;
+        this.jumlah = jumlah;
+        this.harga = harga;
+        
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public float getTipe() {
+        return tipe;
+    }
+
+    public void setTipe(float tipe) {
+        this.tipe = tipe;
+    }
+
+    public float getJumlah() {
+        return jumlah;
+    }
+
+    public void setJumlah(float liter) {
+        this.jumlah = jumlah;
+    }
+
+    public float getHarga() {
+        return harga;
+    }
+
+    public void setHarga(float harga) {
+        this.harga = harga;
+    }
     
+    public float HargaTotal(){
+        return jumlah*harga;
+    } 
 }
